44143572d22e198182ad0ac6dcfe6b69a025526c
Callum Booth <callum@howtomoodle.com>
2018-11-14 13:38:58 +0000

    updated layout, added make a round, added remove person from round

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.scss';
 
 import data from './data.json';
 
+import { ReactComponent as CoffeeIcon } from './icons/coffee-solid.svg';
+import { ReactComponent as TeaPot } from './icons/teapot.svg';
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -17,12 +20,49 @@ class App extends Component {
     }
 
     addPersonToRound(e, person) {
-        if (this.state.round.find(o => o.id === person.id)) {
+        if (this.state.round.indexOf(person.id) !== -1) {
             return;
         }
         this.setState(prevState => ({
             round: [...prevState.round, person.id]
         }));
+    }
+
+    removePersonFromRound(e, person) {
+        if (this.state.round.indexOf(person.id) === -1) {
+            return;
+        }
+        this.setState(prevState => ({
+            round: prevState.round.filter((id, i) => {
+                return id !== person.id
+            })
+        }));
+    }
+
+    makeARound(e, maker) {
+        let updatedPeople = this.state.people.slice();
+        let theRound = this.state.round.slice();
+
+        updatedPeople.map(person => {
+            if (theRound.indexOf(person.id) === -1) {
+                return person;
+            }
+            person.cupsdrank++;
+            if (maker.id === person.id) {
+                person.roundsmade++;
+            }
+
+            return person;
+        });
+
+        this.setState({
+            people: updatedPeople,
+            round: []
+        }, () => {
+            if (typeof(this.props.onChosen) === "function") {
+                this.props.onChosen(this.state.people);
+            }
+        });
     }
 
     chooseTeaMaker = (e) => {
@@ -40,11 +80,12 @@ class App extends Component {
 
             let weight = person.cupsdrank - person.roundsmade;
             
-            if (weight > 1) {
-                for(let i = 0; i < weight; i++) {
+            if (weight > 0) {
+                for(let i = 1; i < weight; i++) {
                     theRound.push(person.id);
                 }
             }
+            return updatedPeople;
         });
         const winner = theRound[this.getRandomInt(theRound.length)];
 
@@ -71,7 +112,7 @@ class App extends Component {
             if (typeof(this.props.onChosen) === "function") {
                 this.props.onChosen(this.state.people);
             }
-        });   
+        });
     }
 
     getRandomInt(max) {
@@ -85,6 +126,16 @@ class App extends Component {
             showModal: false
         });
     }
+
+    getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+      
 
     render() {
         return (
@@ -109,14 +160,55 @@ class App extends Component {
                 
                 <div className="people">
                     {this.state.people.map((person, i) => {
+                        let name = person.name.split(" ");
+                        let image
+                        if (person.image) {
+                            image = person.image;
+                        } else {
+                            image = (<div>
+                                {name.map((word, i) => {
+                                    if (i < 2) {
+                                        return word.substring(0,1);
+                                    }
+                                    return '';
+                                })}
+                            </div>);
+                        }
                         return (
                             <div className="person" key={i}>
-                                Name: {person.name} <br />
-                                Cups drank: {person.cupsdrank} <br />
-                                Rounds Made: {person.roundsmade} <br />
-                                <button className="tf-btn" onClick={(e) => this.addPersonToRound(e, person)}>
-                                    Add to round
-                                </button>
+                                <div className="person-wrapper">
+                                    <div className="profile-image" style={{"backgroundColor": this.getRandomColor()}}>
+                                        {image}
+                                    </div>
+                                    <div className="profile-info">
+                                        <div className="name"><h3>{person.name}</h3></div>
+                                        <div className="cups-info">
+                                            <div className="drank">
+                                                <CoffeeIcon style={{"width": "12px"}}/> {person.cupsdrank} <br />
+                                            </div>        
+                                            <div className="made">
+                                                <TeaPot style={{"width": "12px"}}/> {person.roundsmade} <br />
+                                            </div>
+                                        </div>
+                                        <div style={{"flex": "0 0 100%"}}>
+                                            <button className="tf-btn" onClick={(e) => this.addPersonToRound(e, person)}>
+                                                Add to round
+                                            </button>
+                                        </div>
+                                        {this.state.round.indexOf(person.id) !== -1 ? 
+                                            (
+                                                <div style={{"display": "block"}}>
+                                                    <button className="tf-btn" onClick={(e) => this.makeARound(e, person)}>
+                                                        Make the round
+                                                    </button>
+                                                    <button className="tf-btn" onClick={(e) => this.removePersonFromRound(e, person)}>
+                                                        delete from round
+                                                    </button>
+                                                </div>
+                                            )
+                                        : null}
+                                    </div>
+                                </div>
                             </div>
                         );
                     })}
